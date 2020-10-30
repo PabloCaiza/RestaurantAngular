9266aa73a09d21deeadd81353362fbdd40b9ad5e
Pablo Caiza <pablocuri2015@hotmail.com>
2020-10-29 19:24:41 -0500

    angular router basics

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import {routes} from './app-routing/routes';
-
+import {routes} from './routes';
 
 @NgModule({
-  imports: [CommonModule,RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+
+
+
+
+
