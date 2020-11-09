5e0173259dea6beee271cdd164f10e67a92e0acc
Pablo Caiza <pablocuri2015@hotmail.com>
2020-11-09 11:17:59 -0500

    Promise Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import {FormsModule} from '@angular/forms';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatSelectModule} from '@angular/material/select'
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
@@ -68,7 +68,8 @@ import { LoginComponent } from './login/login.component'
     FormsModule,
     ReactiveFormsModule,
     MatSlideToggleModule,
-    MatSelectModule
+    MatSelectModule,
+    MatProgressSpinnerModule
 
   ],
   entryComponents: [LoginComponent],
