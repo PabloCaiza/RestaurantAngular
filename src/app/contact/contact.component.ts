68b7e8a7b705edb7959cf028b0f668d58efb18e4
Pablo Caiza <pablocuri2015@hotmail.com>
2020-11-12 12:26:28 -0500

    Reactive Forms Part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -13,6 +13,35 @@ export class ContactComponent implements OnInit {
   contactType=ContactType;
   @ViewChild('fform') feedbackFormDirective;
 
+  formsError={
+    'firstname':'',
+    'lastname':'',
+    'telnum':'',
+    'email':''
+  }
+  validationMessages={
+    'firstname':{
+      'required':'First Name is requiered',
+      'minlength':'First Name must be at least 2 character long',
+      'maxlength':'First Name cannot be more than 25 characters long'
+    },
+    'lastname':{
+      'required':'Last Name is requiered',
+      'minlength':'Last Name must be at least 2 character long',
+      'maxlength':'First Name cannot be more than 25 characters long'
+    },
+    'telnum':{
+      'required':'Tel Number is requiered',
+      'pattern':'Tel Number must contain numbers'
+    },
+    'email':{
+      'required':'Email is requiered',
+      'email':'Email not in valid format'
+    }
+    
+
+};
+
   constructor(private fb:FormBuilder) {
     this.createForm();
    }
@@ -32,17 +61,49 @@ export class ContactComponent implements OnInit {
 
     // });
     this.feedbackForm=new FormGroup({
-      firstname: new FormControl('',Validators.required),
-      lastname:new FormControl('',Validators.required),
-      telnum:new FormControl(0,Validators.required),
-      email:new FormControl('',Validators.required),
+      firstname: new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(25) ]),
+      lastname:new FormControl('',[Validators.required,Validators.minLength(2),Validators.maxLength(25) ]),
+      telnum:new FormControl(0,[Validators.required,Validators.pattern("") ]),
+      email:new FormControl('',[Validators.required,Validators.email]),
       agree:new FormControl(false),
       contacttype:new FormControl('None'),
       message:new FormControl('')
+    });
+
+    this.feedbackForm.valueChanges.subscribe(data=>this.onValueChanged(data));
+
+
+   
+
+  }
+  onValueChanged(data?:any) :void{
+    
+    if(!this.feedbackForm)return;
+    
+    const form =this.feedbackForm;
+
+    for(const field in this.formsError){
+      this.formsError[field]='';
+      const control=form.get(field);
+      
+      if(control && control.dirty && !control.valid && control.touched ){
+        
+        
+        const message=this.validationMessages[field];
+       
+        for(const key in control.errors){
+         
+          if(message[key] ){
+            
+            this.formsError[field]+=message[key]+" ";
+            console.log(this.formsError[field]);
+          }
+
+        }
+      }
+
     }
 
-
-    );
 
   }
   onSubmit():void{
